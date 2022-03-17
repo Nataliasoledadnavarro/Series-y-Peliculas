f8561b62c8f6c2688f98e6f2fd66d4282f51b4c9
Natalia Soledad Navarro <nataliasoledadnavarro@gmail.com>
2022-03-17 15:15:44 -0300

    Mejoro ruteo. Incorporo tipo y id a los Links de la Card.

--- src/App.js
+++ src/App.js
@@ -42,18 +42,18 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />}/>{/*muestra series y peliculas que son tendencia.*/}
 
-        <Route path="/peliculas" element={<PeliculasGeneral />} />{/*muestra peliculas: populares/mejores criticas/ a estrenarse/ en cine.*/}
+        <Route path="/movie" element={<PeliculasFiltro />} />{/*muestra peliculas: populares/mejores criticas/ a estrenarse/ en cine.*/}
         <Route path="/peliculas/:filtroPeliculas/page/:paginaPeliculas"element={<PeliculasFiltro />}/>{/*muestra todas las peliculas disponible segun el parametro elegido.*/}
-        <Route path="/peliculas/:idPelicula/info" element={<InfoPelicula />} /> {/*Detalle info de la pelicula elegida*/}
+        <Route path="/movie/:id/info" element={<InfoPelicula />} /> {/*Detalle info de la pelicula elegida*/}
         <Route path="/peliculas/:idPelicula/video" element={<VideoPelicula />}/> {/*Videos de la pelicula elegida*/}
         <Route path="/peliculas/:idPelicula/similar" element={<Similares />} /> {/*Peliculas/Series similares de la pelicula elegida*/}
         <Route path="/peliculas/:idPelicula/reparto" element={<Reparto />} /> {/*Reparto de la serie/pelicula*/}
         <Route path="/persona/:idPelicula/info" element={<PersonaInfo />} /> {/*Detalle del actor/actriz seleccionado*/}
         <Route path="/persona/:idPelicula/creditos"element={<PersonaCreditos />}/> {/*Creditos del actor/actriz seleccionado*/}
 
-        <Route path="/series" element={<SeccionSeries />}/> {/*muestra series: populares/mejores criticas/ al aire*/}
+        <Route path="/tv" element={<SeriesFiltro />}/> {/*muestra series: populares/mejores criticas/ al aire*/}
         <Route path="/series/:filtroSeries/page/:paginaSeries" element={<SeriesFiltro />}/> {/*muestra todas las series disponibles segun el parametro elegido.*/}
-        <Route path="/series/:idSerie/info" element={<InfoSerie />} />  {/*Detalle info de la serie elegida*/}
+        <Route path="/tv/:id/info" element={<InfoSerie />} />  {/*Detalle info de la serie elegida*/}
         <Route path="/series/:idSerie/episodios/:temporadaSerie" element={<EpisodiosSerie />}/> {/*Episodios de la serie elegida*/}
         <Route path="/series/:idSerie/reparto" element={<Reparto />} />{/*Reparto de la serie/pelicula*/}
         <Route path="/series/:idSerie/similares" element={<Similares />} />{/*Reparto de la serie/pelicula*/}
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,8 @@
 import "../styles/components/_Card.scss";
 import img from "../img/imagen-no-encontrada.jpg";
+import { Link } from "react-router-dom";
 
-const Card = ({ resultado }) => {
+const Card = ({ resultado, tipo}) => {
   const mostrarImagen = (resultado) => {
     if (resultado.profile_path) {
       return `https://image.tmdb.org/t/p/w300/${resultado.profile_path}`;
@@ -13,14 +14,19 @@ const Card = ({ resultado }) => {
   };
 
   return (
-    <article key={resultado.id} className="tarjeta">
-      <div className="contenedor-img">
-        <img src={mostrarImagen(resultado)} alt={resultado.title ? resultado.title : resultado.name}></img>
-      </div>
-      <h3 className="titulo-resultado">
-        {resultado.title ? resultado.title : resultado.name}
-      </h3>
-    </article>
+    <Link to={`/${tipo}/${resultado.id}/info`}>
+      <article key={resultado.id} className="tarjeta">
+        <div className="contenedor-img">
+          <img
+            src={mostrarImagen(resultado)}
+            alt={resultado.title ? resultado.title : resultado.name}
+          ></img>
+        </div>
+        <h3 className="titulo-resultado">
+          {resultado.title ? resultado.title : resultado.name}
+        </h3>
+      </article>
+    </Link>
   );
 };
 
